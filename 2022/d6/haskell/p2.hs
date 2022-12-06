0cd92389084818025cbc5a66ae30d25eb3cec881
Aleryc Serrania <aleryc.serrania@ucdconnect.ie>
2022-12-06 08:55:18 +0100

    Add 2022 day 6

---
+++ 2022/d6/haskell/p2.hs
@@ -0,0 +1,33 @@
+import Data.Maybe
+import Data.List (null, take)
+import Data.Set (empty, member, insert)
+import System.IO
+
+main = do
+  input <- getInput
+  print (map (posMessageMarker) input)
+
+posMessageMarker = posMarker 14
+
+posMarker n s = posMarker' s 0
+  where
+    posMarker' s i
+      | length s < n = Nothing
+      | noDup (take n s) = Just (i + n)
+      | otherwise = posMarker' (tail s) (i+1)
+
+noDup l = noDup' l empty
+  where
+    noDup' l u
+      | null l = True
+      | member (head l) u = False
+      | otherwise = noDup' (tail l) (insert (head l) u)
+
+getInput = do
+  done <- isEOF
+  if done
+    then return []
+    else do
+      line <- getLine
+      rest <- getInput
+      return (line : rest)
