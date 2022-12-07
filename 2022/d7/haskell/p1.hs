1396941735c1450fd12ca3349cd336368dc3c70d
Aleryc Serrania <aleryc.serrania@ucdconnect.ie>
2022-12-07 23:00:14 +0100

    Add 2022 day 7

---
+++ 2022/d7/haskell/p1.hs
@@ -0,0 +1,73 @@
+import Data.Maybe
+import Data.List
+import System.IO
+import System.Environment
+
+main = do 
+  input <- getInput
+  args <- getArgs
+  let n = read (head args) :: Int
+  let paths = map reverse . parseFileSystem input $ []
+  let disk =  (foldl insertPath (head . listToTree . head $ paths) paths)
+  let culprits = (filter ((=='d') . first) . filter ((<=n) . third) . flatten . addSize $ disk)
+  print (sum . map third $ culprits)
+
+data Tree a = Node a [Tree a] deriving (Show)
+value (Node v _) = v
+children (Node _ c) = c
+flatten (Node v c) 
+  | null c = [v]
+  | otherwise = [v] ++ (flatten (head c)) ++ concat (map flatten (tail c))
+
+addSize (Node (name, size) children)
+  | isJust size = (Node ('f', name, fromJust size) cr)
+  | otherwise = (Node ('d', name, sum l) cr)
+    where l = (map (\t -> third . value $ t) cr)
+          cr = (map addSize children)
+
+first (a, _, _) = a
+third (_, _, c) = c
+
+listToTree [] = []
+listToTree xs = [Node (head xs) (listToTree' (tail xs))]
+  where 
+    listToTree' ns
+      | null ns = []
+      | otherwise = [Node (head ns) (listToTree' (tail ns))]
+
+insertPath node path = (Node (head path) (mergeChildren (children node) (tail path)))
+  where 
+    mergeChildren c p
+      | null c = listToTree p
+      | (value (head c)) == (head p) = insertPath (head c) (p) : tail c
+      | otherwise = (head c) : mergeChildren (tail c) p
+
+parseFileSystem input wd
+  | null input = []
+  | cmd == "$ cd" = cd rest wd (tail input)
+  | cmd == "$ ls" = files ++ next 
+    where 
+      cmd = take 4 (head input)
+      rest = drop 5 (head input)
+      files = map (:wd) . map (toFile) . takeWhile ((/='$') . head) $ (tail input)
+      next = parseFileSystem (dropWhile ((/='$') . head) $ (tail input)) wd
+
+cd name wd next 
+  | name == ".." = parseFileSystem (next) (tail wd)
+  | otherwise = (dir : wd) : parseFileSystem next (dir : wd)
+    where dir = (name, Nothing)
+
+toFile f 
+  | t == "dir" = (n, Nothing)
+  | otherwise = (n, Just (read t :: Int))
+    where (t, n) = (take sp f, drop (sp+1) f)
+          sp = fromJust (elemIndex ' ' f)
+
+getInput = do
+  done <- isEOF
+  if done
+    then return []
+    else do
+      line <- getLine
+      rest <- getInput
+      return (line : rest)
